0f64f162bbc5cd18e1897a6a4a3df4854cd0ebef
rinaok <rinaok@gmail.com>
2019-10-18 17:59:26 +0300

    show branches

--- webApp/web/pages/repository/repository.js
+++ webApp/web/pages/repository/repository.js
@@ -5,13 +5,20 @@ $(function() { // onload...do
         dataType: 'json',
         timeout: 4000,
         error: function (e) {
-            debugger;
             console.log(e.responseText);
         },
         success: function (r) {
             var branches = r.branches;
             var commits = r.commits;
-            console.log(branches);
+            showBranchesData(branches);
         }
     })
 });
+
+function showBranchesData(branches){
+    var branchesList = $('#branchesList');
+    branchesList.empty();
+    for (var i = 0 ; i < branches.length; i++) {
+        branchesList.append("<li class='list-group-item'>"+branches[i].name+"</li>");
+    }
+}
